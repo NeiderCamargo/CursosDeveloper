18d9b8c732b73abc692b4690caa835c385de48f2
neider-camargo <nfcamargo99@outlook.com>
2023-06-07 14:17:58 -0500

    curso js css html

---
+++ CursoJS_HTML_CSS/JS/1.Introducción/variables.js
@@ -0,0 +1,16 @@
+//String
+var nombre = 'Neider'
+
+//Enteros
+var numero_Entero = 20
+
+//Decimales
+var numero_decimal = 10.5
+
+//booleanos
+var falso = false
+var verdadero = true
+
+//undefined //variables sin definir
+var sin_definir;
+console.log(sin_definir);
